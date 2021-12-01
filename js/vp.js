7a1863a261c2edca33db0225edc7266dc5a11efb
Licsko <varga.sandor@mail.com>
2021-12-01 18:11:47 +0100

    Navbar has been responsived.

--- js/vp.js
+++ js/vp.js
@@ -1,15 +1,18 @@
 const menuButton = document.querySelector('.menu-btn');
 const closeButton = document.querySelector('.close-btn');
 
-const dropdownMenu = document.querySelector('.column');
+const secondNAV = document.querySelector('.nav-2');
+const dropdownMenu = document.querySelector('.nav-2 .nav-list-column');
 
 menuButton.addEventListener('click', function () {
+    secondNAV.classList.remove('elrejt');
     menuButton.classList.add('elrejt');
     closeButton.classList.remove('elrejt');
     dropdownMenu.classList.remove('elrejt');
 });
 
 closeButton.addEventListener('click', function () {
+    secondNAV.classList.add('elrejt');
     menuButton.classList.remove('elrejt');
     closeButton.classList.add('elrejt');
     dropdownMenu.classList.add('elrejt');
